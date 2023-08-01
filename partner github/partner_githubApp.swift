2b46f57e0b171d4e591abde178ed2ffbd840bb9e
Leona Chen <leonachenpiano@gmail.com>
2023-08-01 17:31:40 -0400

    Initial Commit

---
+++ partner github/partner_githubApp.swift
@@ -0,0 +1,17 @@
+//
+//  partner_githubApp.swift
+//  partner github
+//
+//  Created by Leona Chen on 8/1/23.
+//
+
+import SwiftUI
+
+@main
+struct partner_githubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
